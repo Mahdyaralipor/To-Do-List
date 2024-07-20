e3598928ffdd6fa3e8a3cf62bc2a44c1dcf72ea5
mahdyaralipor <mahdyaralipor0@gmail.com>
2024-07-20 15:44:02 +0330

    Fix Select Filter

--- src/components/EditTodoForm.jsx
+++ src/components/EditTodoForm.jsx
@@ -15,12 +15,12 @@ export const EditTodoForm = ({editTodo, task}) => {
       };
   return (
     <form onSubmit={handleSubmit} className="TodoForm">
-      <select value={label} onChange={(e) => setLabel(e.target.value)} className='todo-select'>
+      <select value={label} onChange={(e) => setLabel(e.target.value)} className='select-edit'>
         <option value="normal">Normal</option>
         <option value="important">Important</option>
         <option value="very important">Very Important</option>
       </select>
-    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Update task' />
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="input-edit" placeholder='Update task' />
     <button type="submit" className='todo-btn'>Edit Task</button>
   </form>
   )
